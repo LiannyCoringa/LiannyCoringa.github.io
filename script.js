719af7876616bc2537731470ae66909d958bf784
Lianny Coringa <lianny_costa@hotmail.com>
2023-03-24 15:55:13 -0300

    Colocando o botão de Light Mode

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const clickInButton = () => {
     let header = document.getElementById('header');
     let sobreMim = document.getElementById('sobreMim');
     let habilidades = document.getElementById('habilidades');
+    let button = document.getElementById('button')
     let withMe = document.getElementById('withMe');
     let skills = document.getElementById('skills');
     let footer = document.getElementById('footer');
 
     let background = body.style.backgroundColor = 'black';
     let backgroundHeader = header.style.backgroundColor = 'black';
+    let backgroundButton = button.style.backgroundColor = 'black';
     let backgroundWithMe = withMe.style.backgroundColor = 'black';
     let backgroundSkills = skills.style.backgroundColor = 'black';
     let backgroundFooter = footer.style.backgroundColor = 'rgb(68, 64, 64';
@@ -19,7 +21,7 @@ const clickInButton = () => {
     let colorWithMe = withMe.style.color = 'white';
     let colorSkills = skills.style.color = 'white';
 
-    return(background, backgroundHeader, backgroundWithMe, backgroundSkills, backgroundFooter, colorHeader, colorSobreMim, colorHabilidades, colorWithMe, colorSkills);
+    return(background, backgroundHeader, backgroundButton, backgroundWithMe, backgroundSkills, backgroundFooter, colorHeader, colorSobreMim, colorHabilidades, colorWithMe, colorSkills);
 };
     
     let buttonDark = document.getElementById("darkMode");
